0cd8770d25d2357b546b3791b02145e51320d3ab
tamnguyen02121993 <tamnguyen02121993@gmail.com>
2023-10-24 02:00:21 +0700

    Remove Cors

--- main.py
+++ main.py
@@ -33,11 +33,11 @@ origins = [
     "https://mrcong-nest-api.vercel.app",
 ]
 
-app.add_middleware(CORSMiddleware, 
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],)
+# app.add_middleware(CORSMiddleware, 
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],)
 
 def RecaptchaV3():
     import requests
